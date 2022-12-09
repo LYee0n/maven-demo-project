f78358e3a160d2bfcca2f2e1dde5334b73e312fe
jyasuu <s1002605@pu.edu.tw>
2022-12-09 12:48:00 +0000

    add dumper & restorer

---
+++ core/src/main/java/com/demo/Dumper.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+
+public class Dumper {
+
+    public void dump(){
+        // TODO: Read data from database and output to files
+
+    }
+    
+}
---
+++ core/src/main/java/com/demo/Restorer.java
@@ -0,0 +1,12 @@
+package com.demo;
+
+public class Restorer {
+    
+    public void restore(){
+        // TODO: Read data from files and compare with database then generate different execute sql statements then execute
+
+        
+    }
+    
+}
+
