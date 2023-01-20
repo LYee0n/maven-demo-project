80775654279aa63ac93156f0f2d373ed379e793a
jyasuu <s1002605@pu.edu.tw>
2023-01-20 04:49:16 +0000

    try jmh

--- test/src/test/java/com/demo/AppTest.java
+++ test/src/test/java/com/demo/AppTest.java
@@ -3,18 +3,56 @@ package com.demo;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
 
 /**
  * Unit test for simple App.
  */
+@BenchmarkMode(Mode.All)
+@Warmup(iterations = 3)
+@Measurement(iterations = 10, time = 5, timeUnit = TimeUnit.SECONDS)
+@Threads(8)
+@Fork(2)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
 public class AppTest 
 {
     /**
      * Rigorous Test :-)
      */
     @Test
-    public void shouldAnswerWithTrue()
-    {
+    public void shouldAnswerWithTrue() throws RunnerException {
+        Options options = new OptionsBuilder()
+            .include(AppTest.class.getSimpleName())
+            .output("./Benchmark.log")
+            .build();
+        new Runner(options).run();
         assertTrue( true );
+    }
+
+    
+    @Benchmark
+    public void testStringAdd() {
+        String a = "";
+        for (int i = 0; i < 10; i++) {
+            a += i;
+        }
+        System.out.println(a);
+    }
+
+    @Benchmark
+    public void testStringBuilderAdd() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 10; i++) {
+            sb.append(i);
+        }
+        System.out.println(sb.toString());
     }
 }
