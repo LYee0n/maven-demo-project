ef2c753d8fff6161a2fef54aed0251ffe7f6c689
jyasuu <s1002605@pu.edu.tw>
2023-02-17 14:10:00 +0000

    update

--- maven-example/hello-world/hello-world-child1/src/main/java/com/example/child1/App.java
+++ maven-example/hello-world/hello-world-child1/src/main/java/com/example/child1/App.java
@@ -9,5 +9,31 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        Person person = new Person("Alice", 30, "alice@example.com");
+
+        {
+            String name = person.getName();
+            int age = person.getAge();
+            String email = person.getEmail();
+            System.out.println( name );
+            System.out.println( age );
+            System.out.println( email );
+
+        }
+
+        person.setName("Bob");
+        person.setAge(40);
+        person.setEmail("bob@example.com");
+
+
+        {
+            String name = person.getName();
+            int age = person.getAge();
+            String email = person.getEmail();
+            System.out.println( name );
+            System.out.println( age );
+            System.out.println( email );
+
+        }
     }
 }
